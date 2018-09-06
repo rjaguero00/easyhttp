f4255543ea8716e006e2a42c20ae4dad37458b73
Roberto aguero <rjaguero00@gmail.com>
2018-09-06 15:12:00 -0700

    DELETE method

--- assets/js/app.js
+++ assets/js/app.js
@@ -36,10 +36,19 @@ const data = {
 // });
 
 //Update Post
-http.put('https://jsonplaceholder.typicode.com/posts/1', data, function (err, post) {
+// http.put('https://jsonplaceholder.typicode.com/posts/1', data, function (err, post) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(post);
+//     }
+// });
+
+//Delete Post
+http.delete('https://jsonplaceholder.typicode.com/posts/1', function (err, response) {
     if (err) {
         console.log(err);
     } else {
-        console.log(post);
+        console.log(response);
     }
 });
--- assets/js/easyhttp.js
+++ assets/js/easyhttp.js
@@ -46,3 +46,17 @@ easyHTTP.prototype.put = function (url, data, callback) {
 }
 
 //Make an HTTP DELETE Request 
+easyHTTP.prototype.delete = function (url, callback) {
+    this.http.open('DELETE', url, true);
+
+    let self = this;
+    this.http.onload = function () {
+        if (self.http.status === 200) {
+            callback(null, 'Post Deleted');
+        } else {
+            callback('Error: ' + self.http.status);
+        }
+    }
+
+    this.http.send()
+}
