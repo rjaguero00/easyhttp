9cad675ea4c1d3d794ed9c5cfd0af6ed5e2335b6
Roberto aguero <rjaguero00@gmail.com>
2018-09-06 15:08:24 -0700

    PUT method

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,7 +27,16 @@ const data = {
 };
 
 //Create post
-http.post('https://jsonplaceholder.typicode.com/posts', data, function (err, post) {
+// http.post('https://jsonplaceholder.typicode.com/posts', data, function (err, post) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(post);
+//     }
+// });
+
+//Update Post
+http.put('https://jsonplaceholder.typicode.com/posts/1', data, function (err, post) {
     if (err) {
         console.log(err);
     } else {
