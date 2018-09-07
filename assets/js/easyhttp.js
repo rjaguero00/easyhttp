599f2b445fbebb8f68fc256a961e0542a71af6f9
Roberto aguero <rjaguero00@gmail.com>
2018-09-06 20:11:24 -0700

    new put method

--- assets/js/easyhttp.js
+++ assets/js/easyhttp.js
@@ -90,4 +90,20 @@ class EasyHTTP {
                 .catch(err => reject(err));
         })
     }
+
+    //Make ba HTTP PUT Request
+    put(url, data) {
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                method: 'PUT',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+                .then(res => res.json())
+                .then(data => resolve(data))
+                .catch(err => reject(err));
+        })
+    }
 }
