24e7745a4f84c10106ccb7ac0734032a4d8a40f8
Roberto aguero <rjaguero00@gmail.com>
2018-09-06 20:08:05 -0700

    new post method

--- assets/js/app.js
+++ assets/js/app.js
@@ -59,7 +59,19 @@
 const http = new EasyHTTP;
 
 //Get Users
-http.get('https://jsonplaceholder.typicode.com/users')
+// http.get('https://jsonplaceholder.typicode.com/users')
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err));
+
+//User Data
+const data = {
+    name: 'John',
+    username: 'Johndoe',
+    email: 'jdoe@gmail.com'
+}
+
+//Create Post
+http.post('https://jsonplaceholder.typicode.com/users', data)
     .then(data => console.log(data))
     .catch(err => console.log(err));
 
